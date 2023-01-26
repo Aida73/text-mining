cc0b18592b1f22cfcc2c0092ee5be5b4a25d96aa
Aida Sow <sowaida@ept.sn>
2023-01-26 09:33:55 +0000

    add categories synonymous

--- data_quality_project/dq_project/dq_app/scrapping.py
+++ data_quality_project/dq_project/dq_app/scrapping.py
@@ -23,6 +23,7 @@ It returns a list of elements.
 
 def getCategories(categorie):
     url = 'https://www.rimessolides.com/motscles.aspx'
+    url_syn = 'https://www.rimessolides.com/synonyme.aspx?'
     import random
     # We use a list of fake agents to bypass the limitations of some sites against scrapping
     user_agents_list = [
@@ -34,20 +35,24 @@ def getCategories(categorie):
     if connect():
         res = requests.get(url, headers={
                            'User-Agent': random.choice(user_agents_list)}, params={'m': categorie})
-        #res = requests.get(url)
-        if res.ok:
+        res_syn = requests.get(url_syn, headers={
+            'User-Agent': random.choice(user_agents_list)}, params={'m': categorie})
+        if res.ok and res_syn.ok:
             soup = BeautifulSoup(res.text, 'lxml')
+            soup_syn = BeautifulSoup(res_syn.text, 'lxml')
             result = soup.findAll('a', {'class': 'l-black'})
+            syn_result = soup_syn.findAll('a', {'class': 'l-black'})
             noResult = soup.find('span', {'class': 'medium-bold'})
             if (noResult and noResult.text.startswith("Aucun")):
-                return "Une des catégorie n'est pas correct"
+                return "Une des catégorie n'est pas correcte"
             else:
-                for elt in result:
+                for elt in result+syn_result:
                     categories.append(elt.text)
                 return categories
         else:
             return res.headers
     else:
         print("Verifier votre connexion internet")
+
 
 #print(isinstance(getCategories("médicalejfnrjn"), str))
