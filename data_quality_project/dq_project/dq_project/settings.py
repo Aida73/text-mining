1a171e9ceaae0de17ab92ec38bbbd9f8bcd28840
Aida Sow <sowaida@ept.sn>
2023-01-30 17:43:44 +0000

    add swagger documentation

--- data_quality_project/dq_project/dq_project/settings.py
+++ data_quality_project/dq_project/dq_project/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'dq_app',
+    'rest_framework_swagger',
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
--- data_quality_project/dq_project/dq_project/urls.py
+++ data_quality_project/dq_project/dq_project/urls.py
@@ -17,11 +17,29 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework.documentation import include_docs_urls
+from rest_framework_swagger.views import get_swagger_view
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 from dq_app import views
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Text Mining api",
+        default_version='v1',
+        description="Ceci montre l'ensemble des endpoints disponible pour notre projet",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="sowaida@ept.sn"),
+        #license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/correction', views.DatasetCorrectionView.as_view()),
-    path('api/categorization', views.DatasetCategorizationView.as_view())
+    path('api/categorization', views.DatasetCategorizationView.as_view()),
+    path('', schema_view.with_ui('swagger',
+         cache_timeout=0), name='schema-swagger-ui'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
